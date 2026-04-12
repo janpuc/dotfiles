d0bfbc5af28132e1087b300b6dc4569041834dc3
janpuc <janpuc@proton.me>
2026-04-12 15:02:33 +0200

    feat: add aliases

--- home/dot_config/fish/config.fish
+++ home/dot_config/fish/config.fish
@@ -1,6 +1,22 @@
 set -g fish_greeting
 
+## Abbrs
+
 abbr --add g git
+
+## Aliases
+
+### Kubernetes
+alias k="kubectl"
+alias kx="kubectx"
+alias ku="kubectx -u"
+
+### AWS
+alias ax="awsx"
+alias au="set -e AWS_PROFILE"
+alias ao="assume -ar"
+
+## Config
 
 fish_config theme choose catppuccin-mocha
 
