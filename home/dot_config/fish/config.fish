80dd3c4173cee7f1516ad2613ce0b76c64dace63
janpuc <janpuc@proton.me>
2026-04-12 14:25:18 +0200

    feat: add starship config

---
+++ home/dot_config/fish/config.fish
@@ -0,0 +1,13 @@
+set -g fish_greeting
+
+function starship_transient_prompt_func
+  starship module character
+end
+
+function starship_transient_rprompt_func
+  starship module time
+end
+
+starship init fish | source
+
+enable_transience
