80875d4aebb80ded1bef1ba8da281db4a63c16e5
janpuc <janpuc@proton.me>
2026-04-14 14:15:08 +0200

    feat: switch spicetify to mocha

--- home/.chezmoiscripts/run_onchange_after_spicetify.sh
+++ home/.chezmoiscripts/run_onchange_after_spicetify.sh
@@ -4,6 +4,6 @@ set -eufo pipefail
 
 spicetify backup apply
 spicetify config current_theme catppuccin
-spicetify config color_scheme frappe
+spicetify config color_scheme mocha
 spicetify config inject_css 1 inject_theme_js 1 replace_colors 1 overwrite_assets 1
 spicetify apply
